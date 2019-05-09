40b21cbebee07d99410c3a53e498d8407eadbf5b
ilyaKhim <ilya.himchenko@yandex.ru>
2019-05-09 12:17:35 +0600

    test

--- src/MyArr.java
+++ src/MyArr.java
@@ -1,2 +1,18 @@
+import java.util.ArrayList;
+
 public class MyArr {
+        public static void main(String[] args) {
+            ArrayList<String> arrayList = new ArrayList<>();
+            arrayList.add("A");
+            arrayList.add("B");
+            arrayList.add("C");
+            arrayList.add("D");
+            arrayList.add("E");
+            arrayList.add(1, "А0");
+            System.out.println(arrayList);
+            arrayList.remove("E");
+            arrayList.remove(2);
+            System.out.println(arrayList);
+        }
+
 }
